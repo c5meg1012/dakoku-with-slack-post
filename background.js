dc7028c8a428f5fc73c1255225b9f3ab51f1b9ee
c5meg1012 <12626275+c5meg1012@users.noreply.github.com>
2023-12-06 00:52:07 +0900

    slack に post するようにした

--- background.js
+++ background.js
@@ -2,3 +2,37 @@
 chrome.action.onClicked.addListener((tab) => {
 	chrome.tabs.create({url: 'https://p.st-secure.freee.co.jp/' });
 });
+
+// CORS回避でイベントを受け取ってPOSTする
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	if (!message) {
+		sendResponse({
+			'status': false,
+			'reason': 'message is missing'
+		});
+	} else if (message.contentScriptQuery === 'post') {
+		const data = message.data;
+
+		// TODO: 後でスプレッドシートに移行する
+		fetch('https://hooks.slack.com/services/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(message.data)
+		})
+		.then((response) => {
+			if (response) {
+				sendResponse(response);
+			}
+		})
+		.catch((error) => {
+			sendResponse({
+				'status': false,
+				'reason': 'failed to fetch'
+			});
+		});
+	}
+
+	return true;
+});
--- content.js
+++ content.js
@@ -1,3 +1,32 @@
+// Slack post
+const postToSlack = (emoji) => {
+	const data = { 'text': emoji };
+
+	// ここでPOSTするとCORSで引っかかるのでbackgroundに渡す
+	chrome.runtime.sendMessage({
+		contentScriptQuery: 'post',
+		data: data
+	});
+};
+
+const setEmoji = (isInOffice, isSyukkin) => {
+	let emoji = '';
+
+	if (isInOffice === true) {
+		emoji = ':butsuri_syussya: ';
+	} else if (isInOffice === false) {
+		emoji = ':ronri_syussya: ';
+	}
+
+	if (isSyukkin) {
+		emoji = emoji + ':ohayougozaimasu:';
+	} else {
+		emoji = emoji + ':otsucurry:';
+	}
+
+	return emoji;
+};
+
 // 距離を測る関数
 const R = Math.PI / 180;
 const getDistance = (lat1, lng1, lat2, lng2) => {
@@ -5,6 +34,7 @@ const getDistance = (lat1, lng1, lat2, lng2) => {
   lng1 *= R;
   lat2 *= R;
   lng2 *= R;
+
   return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
 }
 
@@ -13,11 +43,15 @@ const successGetPosition = (position) => {
 	let isInOffice = false;
 
 	// 大崎ネストとの距離(km)
-	const distance = getDistance(position.coords.latitude, position.coords.longitude, 35.6223771,139.7248845);
-	// if ((distance * 1000) < 200) { 大崎ネストに戻ったらこちらを利用する
-	if ((distance * 1000) > 200) {
+	// const distance = getDistance(position.coords.latitude, position.coords.longitude, 35.6223771,139.7248845); // 大崎ネストに戻ったらこちらを利用する
+	const distance = getDistance(position.coords.latitude, position.coords.longitude, 36.33556235419869, 138.6371057406137); // 軽井沢コテージとの距離
+
+	if ((distance * 1000) < 200) {
 		isInOffice = true;
 	}
+
+	const emoji = setEmoji(isInOffice, true);
+	postToSlack(emoji);
 
 	// 出退勤ボタンからのアクション
 	const element = document.querySelector('button.vb-button--appearancePrimary');
@@ -31,11 +65,14 @@ const successGetPosition = (position) => {
 		element.addEventListener('click', (() => {
 			console.log('退勤ボタンが押されました');
 		}));
-	}
+	// }
 };
 
 // 位置情報が取れなかったら
-const errorGetPosition = (position) => {	
+const errorGetPosition = (position) => {
+	const emoji = setEmoji(null, true);
+	postToSlack(emoji);
+
 	// 出退勤ボタンからのアクション
 	const element = document.querySelector('button.vb-button--appearancePrimary');
 
