70c19275b19dd9625c8d389dbff1d40ba51e5689
c5meg1012 <12626275+c5meg1012@users.noreply.github.com>
2023-12-05 20:12:49 +0900

    クリックイベントを追加

--- content.js
+++ content.js
@@ -1,11 +1,16 @@
 window.onload = () => {
 	setTimeout(() => {
 		const element = document.querySelector('button.vb-button--appearancePrimary');
+
 		if (element.children[0].textContent === '出勤') {
-			console.log('出勤ボタン');
+			element.addEventListener('click', (() => {
+				console.log('出勤ボタンが押されました');
+			}));
 		}
 		if (element.children[0].textContent === '退勤') {
-			console.log('退勤ボタン');
+			element.addEventListener('click', (() => {
+				console.log('退勤ボタンが押されました');
+			}));
 		}
 	}, 1000 );
 };
