a60effbfbf032007660ef650b3addac2546c23d6
c5meg1012 <12626275+c5meg1012@users.noreply.github.com>
2023-12-06 10:04:46 +0900

    位置情報をSlackの画面まで持ってきた

--- background.js
+++ background.js
@@ -1,36 +1,52 @@
 // アイコンをクリックしたら人事労務を開く
 chrome.action.onClicked.addListener((tab) => {
-	chrome.tabs.create({url: 'https://p.st-secure.freee.co.jp/' });
+	chrome.tabs.create({ url: 'https://p.st-secure.freee.co.jp/' });
 });
 
-// CORS回避でイベントを受け取ってPOSTする
+const slackFunc = (args) => {
+	const isInOffice = args.isInOffice;
+	const isSyukkin = args.isSyukkin;
+	let emoji = '';
+
+	if (isSyukkin) {
+		if (isInOffice === true) {
+			emoji = ':butsuri_syussya: ';
+		} else if (isInOffice === false) {
+			emoji = ':ronri_syussya: ';
+		}
+
+		emoji = emoji + ':ohayougozaimasu:';
+	} else {
+		if (isInOffice === true) {
+			emoji = ':butsuri_taikin: ';
+		} else if (isInOffice === false) {
+			emoji = ':ronri_taikin: ';
+		}
+
+		emoji = emoji + ':otsucurry:';
+	}
+
+	// Slack に post する
+	console.log('emoji: ' + emoji);
+}
+
+// イベントを受け取る
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (!message) {
 		sendResponse({
 			'status': false,
 			'reason': 'message is missing'
 		});
-	} else if (message.contentScriptQuery === 'post') {
-		const data = message.data;
-
-		// TODO: 後でスプレッドシートに移行する
-		fetch('https://hooks.slack.com/services/', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(message.data)
-		})
-		.then((response) => {
-			if (response) {
-				sendResponse(response);
-			}
-		})
-		.catch((error) => {
-			sendResponse({
-				'status': false,
-				'reason': 'failed to fetch'
-			});
+	} else {
+		// 出勤/退勤ボタンのイベント
+		chrome.tabs.create({ url: 'https://app.slack.com/client/' }, (tab) => {
+			chrome.scripting.executeScript({
+				target: { tabId: tab.id },
+        func: slackFunc,
+        args: [message],
+			}, () => {
+				sendResponse('slackOpened');
+			})
 		});
 	}
 
---
+++ content_freee_hr.js
@@ -0,0 +1,73 @@
+// 距離を測る関数
+const R = Math.PI / 180;
+const getDistance = (lat1, lng1, lat2, lng2) => {
+  lat1 *= R;
+  lng1 *= R;
+  lat2 *= R;
+  lng2 *= R;
+
+  return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+}
+
+// 位置情報を取得完了してから処理を開始
+const successGetPosition = (position) => {
+	let isInOffice = false;
+	const isSyukkin = true; // ボタンのテストの時は削除
+
+	// 大崎ネストとの距離(km)
+	// const distance = getDistance(position.coords.latitude, position.coords.longitude, 35.6223771,139.7248845); // 大崎ネストに戻ったらこちらを利用する
+	const distance = getDistance(position.coords.latitude, position.coords.longitude, 36.33556235419869, 138.6371057406137); // 軽井沢コテージとの距離
+
+	if ((distance * 1000) < 200) {
+		isInOffice = true;
+	}
+
+	// 出退勤ボタンからのアクション
+	// const element = document.querySelector('button.vb-button--appearancePrimary');
+
+	// if (element.children[0].textContent === '出勤') {
+	// 	element.addEventListener('click', (() => {
+	// 		console.log('出勤ボタンが押されました');
+	// 	}));
+	// }
+	// if (element.children[0].textContent === '退勤') {
+	// 	element.addEventListener('click', (() => {
+	// 		console.log('退勤ボタンが押されました');
+	// 	}));
+	// }
+
+	// 出退勤の情報を渡してイベントを送る
+	chrome.runtime.sendMessage({
+		isSyukkin: isSyukkin,
+		isInOffice: isInOffice,
+	}, (res) => {
+		console.log(res);
+	});
+};
+
+// 位置情報が取れなかったら
+const errorGetPosition = (position) => {
+	// 出退勤ボタンからのアクション
+	// const element = document.querySelector('button.vb-button--appearancePrimary');
+
+	// if (element.children[0].textContent === '出勤') {
+	// 	element.addEventListener('click', (() => {
+	// 		console.log('出勤ボタンが押されました');
+	// 	}));
+	// }
+	// if (element.children[0].textContent === '退勤') {
+	// 	element.addEventListener('click', (() => {
+	// 		console.log('退勤ボタンが押されました');
+	// 	}));
+	// }
+
+	// 出退勤の情報を渡してイベントを送る
+	chrome.runtime.sendMessage({
+		isSyukkin: isSyukkin,
+		isInOffice: null,
+	}, (res) => {
+		console.log(res);
+	});
+};
+
+navigator.geolocation.getCurrentPosition(successGetPosition, errorGetPosition);
